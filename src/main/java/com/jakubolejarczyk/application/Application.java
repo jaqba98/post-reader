00a58a992cebb7b1dc291d729c899fbd21c43130
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-25 16:52:20 +0200

    feat: Create post domain builder.

--- src/main/java/com/jakubolejarczyk/application/Application.java
+++ src/main/java/com/jakubolejarczyk/application/Application.java
@@ -1,7 +1,9 @@
 package com.jakubolejarczyk.application;
 
 import com.jakubolejarczyk.infrastructure.ReadPostsService;
+import com.jakubolejarczyk.model.domain.PostDomainModel;
 import com.jakubolejarczyk.model.dto.PostDtoModel;
+import com.jakubolejarczyk.service.builder.PostDomainBuilder;
 import com.jakubolejarczyk.service.builder.PostDtoBuilder;
 
 import java.util.List;
@@ -9,17 +11,20 @@ import java.util.List;
 public class Application {
     private final ReadPostsService readPostsService;
     private final PostDtoBuilder postDtoBuilder;
+    private final PostDomainBuilder postDomainBuilder;
 
     public Application() {
         this.readPostsService = new ReadPostsService();
         this.postDtoBuilder = new PostDtoBuilder();
+        this.postDomainBuilder = new PostDomainBuilder();
     }
 
     public void run() {
         String posts = readPostsService.readPosts();
         List<PostDtoModel> postsDto = postDtoBuilder.build(posts);
-        postsDto.forEach(postDto -> {
-            System.out.println(postDto.getId());
+        List<PostDomainModel> postsDomain = postDomainBuilder.build(postsDto);
+        postsDomain.forEach(postDomain -> {
+            System.out.println(postDomain.getBody());
         });
     }
 }
