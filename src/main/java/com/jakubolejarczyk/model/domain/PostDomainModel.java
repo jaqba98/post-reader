441c18da9941912ea5d93fb2cb1ef45b1fe9fed6
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-25 14:46:23 +0200

    feat: Create base, domain and dto model for post.

---
+++ src/main/java/com/jakubolejarczyk/model/domain/PostDomainModel.java
@@ -0,0 +1,6 @@
+package com.jakubolejarczyk.model.domain;
+
+import com.jakubolejarczyk.model.base.PostBaseModel;
+
+public class PostDomainModel extends PostBaseModel {
+}
---
+++ src/main/java/com/jakubolejarczyk/model/dto/PostDtoModel.java
@@ -0,0 +1,5 @@
+package com.jakubolejarczyk.model.dto;
+
+import com.jakubolejarczyk.model.base.PostBaseModel;
+
+public class PostDtoModel extends PostBaseModel {}
