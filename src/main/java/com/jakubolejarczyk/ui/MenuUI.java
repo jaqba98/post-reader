4fdafab5ceb426e148df10d1d0ff1c1bb751f197
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-27 11:14:02 +0200

    fix: Menu ui

--- src/main/java/com/jakubolejarczyk/ui/MenuUI.java
+++ src/main/java/com/jakubolejarczyk/ui/MenuUI.java
@@ -4,9 +4,9 @@ import com.jakubolejarczyk.model.ui.UIModel;
 
 public class MenuUI implements UIModel {
     public void draw() {
-        System.out.println("########################################");
-        System.out.println("# POST READER v0.1 by Jakub Olejarczyk #");
-        System.out.println("########################################");
+        System.out.println("##################################################");
+        System.out.println("#      POST READER v0.1 by Jakub Olejarczyk      #");
+        System.out.println("##################################################");
         System.out.println("1) Posts");
         System.out.println("0) Exit program");
         System.out.print("Select API to load: ");
