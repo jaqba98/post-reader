96f6bb596d0a78db10e7711bba72eac1c548f579
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-04-27 13:20:46 +0200

    feat: Add logic to fetch api to the posts logic.

--- src/main/java/com/jakubolejarczyk/logic/PostsLogic.java
+++ src/main/java/com/jakubolejarczyk/logic/PostsLogic.java
@@ -1,9 +1,15 @@
 package com.jakubolejarczyk.logic;
 
+import com.jakubolejarczyk.infrastructure.FetchApiService;
+import lombok.AllArgsConstructor;
+import lombok.val;
+
+@AllArgsConstructor
 public class PostsLogic {
+    private final FetchApiService fetchApiService;
+
     public void run() {
-        System.out.println();
-        System.out.println("Posts");
-        System.out.println();
+        val api = fetchApiService.fetch("posts");
+        System.out.println(api);
     }
 }
